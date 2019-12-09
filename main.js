4d5406c7812deb05559d652c3bfb54ca50bc7b16
Ashutosh Panda <ashu.juga99@gmail.com>
2019-12-09 23:26:27 +0530

    Delete req

--- main.js
+++ main.js
@@ -1,26 +1,63 @@
 // GET REQUEST
+// function getTodos() {
+//   axios({
+//     method: 'get',
+//     url: 'http://jsonplaceholder.typicode.com/todos',
+//     params: {
+//       _limit: 5
+//     }
+//   })
+//     .then(res => showOutput(res))
+//     .catch(err => console.error(err));
+// }
+
 function getTodos() {
-  axios({
-    method: 'get',
-    url: 'http://jsonplaceholder.typicode.com/todos'
-  })
-    .then(res => console.log(res.data, res.method))
+  axios
+    .get('http://jsonplaceholder.typicode.com/todos', { params: { _limit: 5 } })
+    //can add the limit in url with http://jsonplaceholder.typicode.com/todos?_limit=5
+    //can ommit get as axios make get request by default
+    .then(res => showOutput(res))
     .catch(err => console.error(err));
 }
 
 // POST REQUEST
 function addTodo() {
-  console.log('POST Request');
+  axios
+    .post('http://jsonplaceholder.typicode.com/todos', {
+      title: 'New ToDo',
+      completed: false
+    })
+    .then(res => showOutput(res))
+    .catch(err => console.error(err));
 }
 
-// PUT/PATCH REQUEST
+// PUT REQUEST
 function updateTodo() {
-  console.log('PUT/PATCH Request');
+  axios
+    .put('http://jsonplaceholder.typicode.com/todos/1', {
+      title: 'Updated ToDo',
+      completed: true
+    })
+    .then(res => showOutput(res))
+    .catch(err => console.error(err));
+}
+// PATCH Request
+function patchTodo() {
+  axios
+    .patch('http://jsonplaceholder.typicode.com/todos/1', {
+      title: 'Updated ToDo',
+      completed: true
+    })
+    .then(res => showOutput(res))
+    .catch(err => console.error(err));
 }
 
 // DELETE REQUEST
 function removeTodo() {
-  console.log('DELETE Request');
+  axios
+    .delete('http://jsonplaceholder.typicode.com/todos/1')
+    .then(res => showOutput(res))
+    .catch(err => console.error(err));
 }
 
 // SIMULTANEOUS DATA
@@ -92,6 +129,7 @@ function showOutput(res) {
 document.getElementById('get').addEventListener('click', getTodos);
 document.getElementById('post').addEventListener('click', addTodo);
 document.getElementById('update').addEventListener('click', updateTodo);
+document.getElementById('updatepatch').addEventListener('click', patchTodo);
 document.getElementById('delete').addEventListener('click', removeTodo);
 document.getElementById('sim').addEventListener('click', getData);
 document.getElementById('headers').addEventListener('click', customHeaders);
